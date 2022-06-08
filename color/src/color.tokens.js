141049486c2976d7878ec7e5c57d16d785d69d00
josedjesus-morales <jose.morales@televisatim.com>
2022-06-08 22:01:25 +0000

    chore style-dictionary output

--- color/src/color.tokens.js
+++ color/src/color.tokens.js
@@ -1,9 +1,27 @@
 /**
  * Do not edit directly
- * Generated on Wed, 08 Jun 2022 05:51:28 GMT
+ * Generated on Wed, 08 Jun 2022 22:01:24 GMT
  */
 
-export const StarterColorBg = "#e7e0e0";
-export const StarterColorInteraction = "#b51e0d";
-export const StarterColorGray = "#e7e0e0";
-export const StarterColorWhite = "#ffffff";
+export const StarterColorWhite = "#fcfcfb";
+export const StarterColorBlack = "#0a0b0b";
+export const StarterColorPrimary20 = "#5effcd";
+export const StarterColorPrimary40 = "#2bffbd";
+export const StarterColorPrimary60 = "#00ffb0";
+export const StarterColorPrimary80 = "#03865d";
+export const StarterColorPrimary100 = "#026445";
+export const StarterColorComplementary20 = "#ff5e90";
+export const StarterColorComplementary40 = "#ff2b6d";
+export const StarterColorComplementary60 = "#ff004f";
+export const StarterColorComplementary80 = "#e7044a";
+export const StarterColorComplementary100 = "#660020";
+export const StarterColorBreaking20 = "#e12d05";
+export const StarterColorBreaking40 = "#6b1400";
+export const StarterColorWarning20 = "#ffc74d";
+export const StarterColorWarning40 = "#9a6c09";
+export const StarterColorInfo20 = "#0c8bb0";
+export const StarterColorInfo40 = "#014f79";
+export const StarterColorLightgreen = "#e1ffcc";
+export const StarterColorSoftgreen = "#bbffaa";
+export const StarterColorColdgray = "#3c5764";
+export const StarterColorNeutralgray = "#404040";
